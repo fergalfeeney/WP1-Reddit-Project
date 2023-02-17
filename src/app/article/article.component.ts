af42cf895a629369e34c5392d51de231a815eb2b
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-02-17 18:22:44 +0000

    extracted model out into seperate class

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,14 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit{
 
-votes:number;
-title:string;
-link:string;
+article:Article;
 
   constructor() {
-    this.title="Angular";
-    this.link="https://angular.io";
-    this.votes=10;
+    this.article= new Article("Angular", "https://angular.io, 10")
   }
 
   ngOnInit() {
@@ -22,12 +19,12 @@ link:string;
   }
 
   voteUp():boolean{
-this.votes ++;
+this.article.voteUp();
 return false;
   }
 
   voteDown():boolean{
-this.votes --;
+this.article.voteDown();
 return false;
   }
 }
