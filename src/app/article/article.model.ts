991aeece9da2c24e9a9cebef480dc409b945b626
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-02-17 18:50:51 +0000

    made url look neater

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -1,21 +1,31 @@
 export class Article {
-votes:number;
-title:string;
-link:string;
+    votes: number;
+    title: string;
+    link: string;
 
-  constructor(title:string, link:string, votes?:number) {
-    this.title=title;
-    this.link=link;
-    this.votes=votes || 10;
-  }
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 10;
+    }
 
-  voteUp(){
-    this.votes ++;
-    
-      }
-    
-      voteDown(){
-    this.votes --;
-  
-      }
+    voteUp() {
+        this.votes++;
+
+    }
+
+    voteDown() {
+        this.votes--;
+
+    }
+
+    domain(): string {
+        try {
+            const link: string = this.link.split('//')[1];
+            return link.split('/')[0];
+        }
+        catch (err) {
+            return null;
+        }
+    }
 }
