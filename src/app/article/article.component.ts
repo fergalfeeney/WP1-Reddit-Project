34e385c8b72a9f75553e4be8ee513017ac6fe1f5
fergalfeeney <98536841+fergalfeeney@users.noreply.github.com>
2023-02-17 18:02:07 +0000

    vote up and vote down articles that are submitted

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit{
 
+votes:number;
+title:string;
+link:string;
+
+  constructor() {
+    this.title="Angular";
+    this.link="https://angular.io";
+    this.votes=10;
+  }
+
+  ngOnInit() {
+
+  }
+
+  voteUp():boolean{
+this.votes ++;
+return false;
+  }
+
+  voteDown():boolean{
+this.votes --;
+return false;
+  }
 }
+
